1dd4d52116b986dc2f3f865d4e874115b803957e
razanalsunaidi <razanalsunedi@gmail.com>
2024-10-20 15:35:25 +0300

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Razan Alsunaidi on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
